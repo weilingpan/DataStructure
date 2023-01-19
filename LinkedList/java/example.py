6fe405de0178afd8996562b7c470ad49c9d9ec93
Regina <42767268+weilingpan@users.noreply.github.com>
2023-01-19 14:34:54 +0800

    learn: java linked list

---
+++ LinkedList/java/example.py
@@ -0,0 +1,13 @@
+// Import the LinkedList class
+import java.util.LinkedList;
+
+public class Main {
+  public static void main(String[] args) {
+    LinkedList<String> members = new LinkedList<String>();
+    members.add("Amy");
+    members.add("Andy");
+    members.add("Joy");
+    members.add("Lily");
+    System.out.println(members);
+  }
+}
